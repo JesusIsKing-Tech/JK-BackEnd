7e8b13830b5bfc0801e0e0ff107e70ed9ece14aa
KauaVidal <kaua.magalhaes@sptech.school>
2024-08-28 20:53:50 -0300

    feature/crud usuarios

--- jesus-king-tech/src/main/java/com/api/jesus_king_tech/cadastro_login/UsuarioController.java
+++ jesus-king-tech/src/main/java/com/api/jesus_king_tech/cadastro_login/UsuarioController.java
@@ -1,11 +1,14 @@
 package com.api.jesus_king_tech.cadastro_login;
 
+import com.api.jesus_king_tech.cadastro_login.JavaMailRecuperacaoSenha.JavaMail;
 import com.api.jesus_king_tech.util.EmailUtil;
 import com.api.jesus_king_tech.util.PasswordUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
@@ -42,4 +45,36 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body("Usuário cadastrado com sucesso");
     }
+
+    @GetMapping
+    public List<Usuario> listarUsuarios(){
+        return usuarioRepository.findAll();
+    }
+
+    @PostMapping("/recuperar-senha")
+    public ResponseEntity<String> recuperarSenha(@RequestBody String emailUsuario){
+        System.out.println(emailUsuario);
+
+        if (!usuarioRepository.existsByEmail(emailUsuario)){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Email não cadastrado");
+        }
+        JavaMail.sendEmail(emailUsuario);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("Email enviado com sucesso");
+    }
+
+    @PostMapping("/recuperar-senha/validar-codigo")
+    public ResponseEntity<String> validarCodigoRecuperacaoSenha(@RequestBody String codigo){
+        if (codigo.equals(JavaMail.getCode())){
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("Código válido");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Código inválido");
+    }
+
+
+
+
 }
