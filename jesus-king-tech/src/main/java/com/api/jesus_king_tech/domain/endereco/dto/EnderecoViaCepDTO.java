537dc86d494e7e780f19049916ac66433dc18cba
Renan Alves Rocha <143151166+renanroccha@users.noreply.github.com>
2024-10-30 00:15:16 -0300

    Update EnderecoViaCepDTO.java

--- jesus-king-tech/src/main/java/com/api/jesus_king_tech/domain/endereco/dto/EnderecoViaCepDTO.java
+++ jesus-king-tech/src/main/java/com/api/jesus_king_tech/domain/endereco/dto/EnderecoViaCepDTO.java
@@ -11,16 +11,15 @@ import lombok.*;
 @Builder
 public class EnderecoViaCepDTO {
 
-    @NotBlank(message = "O campo CEP é obrigatório.")
+    
     private String cep;
-    @NotBlank(message = "O campo logradouro é obrigatório.")
+   
     private String logradouro;
-    @NotBlank(message = "O campo bairro é obrigatório.")
+  
     private String bairro;
-    @NotBlank(message = "O campo localidade é obrigatório.")
+  
     private String localidade;
-    @NotBlank(message = "O campo UF é obrigatório.")
-    @Pattern(regexp = "[A-Z]{2}", message = "UF deve ser um código de 2 letras.")
+    
     private String uf;
 
 
