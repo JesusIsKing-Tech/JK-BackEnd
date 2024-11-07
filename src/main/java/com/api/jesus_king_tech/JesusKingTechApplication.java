8bf63a2f2112183db4b31cd630a238dce634e420
KauaVidal <kaua.magalhaes@sptech.school>
2024-11-07 01:45:54 -0300

    Mudando projeto para raiz

--- src/main/java/com/api/jesus_king_tech/JesusKingTechApplication.java
+++ src/main/java/com/api/jesus_king_tech/JesusKingTechApplication.java
@@ -15,9 +15,9 @@ public class JesusKingTechApplication {
 		Dotenv dotenv = Dotenv.configure().load();
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
 		System.setProperty("EMAIL_EMAIL", dotenv.get("EMAIL_EMAIL"));
 		System.setProperty("EMAIL_SENHA", dotenv.get("EMAIL_SENHA"));
-
 
 		SpringApplication.run(JesusKingTechApplication.class, args);
 	}
