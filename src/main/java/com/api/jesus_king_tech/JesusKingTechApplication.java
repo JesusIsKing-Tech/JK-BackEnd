7a9483667463b7d641881a3d854f7d5b7f988a9b
KauaVidal <kaua.magalhaes@sptech.school>
2024-11-08 01:08:31 -0300

    tirando necessidade do .env para prod

--- src/main/java/com/api/jesus_king_tech/JesusKingTechApplication.java
+++ src/main/java/com/api/jesus_king_tech/JesusKingTechApplication.java
@@ -12,12 +12,18 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class JesusKingTechApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-		System.setProperty("EMAIL_EMAIL", dotenv.get("EMAIL_EMAIL"));
-		System.setProperty("EMAIL_SENHA", dotenv.get("EMAIL_SENHA"));
+//		Dotenv dotenv = Dotenv.configure().load();
+//		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+//		System.setProperty("EMAIL_EMAIL", dotenv.get("EMAIL_EMAIL"));
+//		System.setProperty("EMAIL_SENHA", dotenv.get("EMAIL_SENHA"));
+
+		System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
+		System.setProperty("DB_HOST", System.getenv("DB_HOST"));
+		System.setProperty("EMAIL_EMAIL", System.getenv("EMAIL_EMAIL"));
+		System.setProperty("EMAIL_SENHA", System.getenv("EMAIL_SENHA"));
 
 		SpringApplication.run(JesusKingTechApplication.class, args);
 	}
