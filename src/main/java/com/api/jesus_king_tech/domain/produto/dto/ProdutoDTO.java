c559eee83b1728ce057be8192f3f47127ba3ad38
renanroccha <renan.rocha@sptech.school>
2024-11-13 20:05:12 -0300

    Produto Correto

--- src/main/java/com/api/jesus_king_tech/domain/produto/dto/ProdutoDTO.java
+++ src/main/java/com/api/jesus_king_tech/domain/produto/dto/ProdutoDTO.java
@@ -2,55 +2,21 @@ package com.api.jesus_king_tech.domain.produto.dto;
 
 import com.api.jesus_king_tech.domain.produto.categoria.dto.CategoriaDTO;
 import com.api.jesus_king_tech.domain.produto.tipo.dto.TipoDTO;
+import lombok.*;
 
 import java.time.LocalDate;
 
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProdutoDTO {
 
     private Integer id;
-    private Double peso;
     private LocalDate dataEntrada;
+    private Double peso;
     private CategoriaDTO categoria;
     private TipoDTO tipo;
 
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Double getPeso() {
-        return peso;
-    }
-
-    public void setPeso(Double peso) {
-        this.peso = peso;
-    }
-
-    public LocalDate getDataEntrada() {
-        return dataEntrada;
-    }
-
-    public void setDataEntrada(LocalDate dataEntrada) {
-        this.dataEntrada = dataEntrada;
-    }
-
-    public CategoriaDTO getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(CategoriaDTO categoria) {
-        this.categoria = categoria;
-    }
-
-    public TipoDTO getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoDTO tipo) {
-        this.tipo = tipo;
-    }
 }
